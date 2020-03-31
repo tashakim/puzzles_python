a4c3884e402f8a2a9017d6b2fdc9611e20da9d85
Tasha Kim <jae_young_kim@brown.edu>
2020-03-30 22:50:24 -0400

    Testing

--- add.py
+++ add.py
@@ -1,5 +1,13 @@
 def add(x,y):
 	return x+y
 
+def test():
+	assert add(1,3) == 4, "Test failed"
+	assert add(0,0) == 0, "Test failed"
+	assert add(1,2) == 1, "Test huh" #wrong assertion!
+
+
+
+
 if __name__ == "__main__":
-	print(add(1,3))
+	test()
