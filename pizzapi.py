5d8c75e13dd4695a092c176818a740445a8e9720
Tasha Kim <jae_young_kim@brown.edu>
2020-03-30 23:08:30 -0400

    dominos

--- pizzapi.py
+++ pizzapi.py
@@ -9,3 +9,8 @@ menu = store.get_menu()
 menu.search(Name = "Coke")
 
 order = Order(store, customer, address)
+
+#you also need to add a payment method
+
+card = PaymentObject("10000000000", "0000", "000", "00000")
+order.place(card)
