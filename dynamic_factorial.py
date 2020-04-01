795376bbacad655b6e0b847a46d836d6fc9ea448
Tasha Kim <jae_young_kim@brown.edu>
2020-03-31 23:50:52 -0400

    March31

--- dynamic_factorial.py
+++ dynamic_factorial.py
@@ -1,10 +1,19 @@
 def dynamic_factorial(n):
 	fact = []
 	fact.append(1)
+	if n == 0:
+		return fact.pop()
 	fact.append(1) 
+	if n==1:
+		return fact.pop()
 	for i in range(2, n+1):
 		fact.insert(i, i*fact[i-1])
 	return fact.pop()
+
+def test():
+	assert dynamic_factorial(3) == 6, "Test failed"
+	assert dynamic_factorial(0) == 1, "Test failed"
+	assert dynamic_factorial(1) == 1, "Test failed"
 
 if __name__ == "__main__":
 	print(dynamic_factorial(2))
