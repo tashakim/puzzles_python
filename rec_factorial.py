38d0cda7f4cd7dcf373b69d21b6d472305a15d3a
Tasha Kim <jae_young_kim@brown.edu>
2020-03-31 23:42:41 -0400

    recursive

--- rec_factorial.py
+++ rec_factorial.py
@@ -1,4 +1,6 @@
 def rec_factorial(n):
+	if n == 0:
+		return 1
 	return helper(n)
 
 def helper(n):
@@ -6,8 +8,13 @@ def helper(n):
 		return 1
 	if n>1:
 		return n*helper(n-1)
+
+def test():
+	assert rec_factorial(3) == 6, "Test failed"
+	assert rec_factorial(0) == 1, "Test failed"
+	assert rec_factorial(1) == 1, "Test failed"
 	
 
 if __name__ == "__main__":
 	print(rec_factorial(2))
-	assert rec_factorial(3) == 6, "Test failed"
+	test()
