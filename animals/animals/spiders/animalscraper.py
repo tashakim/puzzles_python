49fb769893082692f9bdbc1beecff86a6e62d0aa
Tasha Kim <jae_young_kim@brown.edu>
2020-04-04 23:44:53 -0400

    April 4

---
+++ animals/animals/spiders/animalscraper.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class Spider(scrapy.Spider):
+	name = "animal_spider"
+	url = ["https://www.reddit.com/r/animals/"]
+
+	def parse(self, response):
+		links = response.xpath("//img/@src")
+		html = ""
+
+		for link in links:
+			url = link.get()
+
+			if any(extension in url for extension in [".jpg"]):
+				html += """<a href = "{url}"
+				target = "_blank">
+				<img src="{url}" height = "33%"
+				width = "33%">
+				<a/>""".format(url = url)
+
+				with open("animal_images.html", "a") as page:
+					page.write(html)
+					page.close()
