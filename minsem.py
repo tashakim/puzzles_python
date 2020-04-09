e55be2a475993c1e1d7217ef8dc14b7946d95dd3
Tasha Kim <jae_young_kim@brown.edu>
2020-04-08 22:32:09 -0400

    error-ridden

---
+++ minsem.py
@@ -0,0 +1,21 @@
+def minSem(g):
+	"""Purpose:
+	Input: A directed acyclic graph (DAG) with courses
+	Output: Integer, minimum number of semesters to complete the curriculum.
+	"""
+	for i in g.iterVertices():
+		i._vertex = 0
+		i._semesters = 0
+	for i in g.iterVertices:
+		i._indegree = size(i.incidentEdges())
+		if i._indegree == 0:
+			Queue.append(i)
+	while Queue != None:
+		elem = Queue.pop(0)
+		for i in elem.incidentVertices():
+			i._indegree -= 1
+			i._semesters = 1 + elem._semesters
+			if i._indegree == 0:
+				Queue.append(i)
+		minimum = max(semesters)
+	return minimum
