cf221fdfa0a9c0ddcf156821956a6eeb3f3a0b85
Tasha Kim <jae_young_kim@brown.edu>
2020-04-11 22:44:35 -0400

    merge sort

---
+++ mergesort.py
@@ -0,0 +1,32 @@
+def mergeSort(array):
+	if len(array) > 1:
+		left = array[:len(array)//2]
+		right  = array[len(array)//2:]
+
+		mergeSort(left)
+		mergeSort(right)
+
+		i=j=k=0
+
+		while iter1 < len(left) and iter2 < len(right):
+			if left[iter1] < right[iter2]:
+				array[k] = left[iter1]
+				iter += 1
+
+			else:
+				array[k] = right[iter2]
+				iter2 += 1
+			k += 1
+
+		while iter1 < len(left):
+			array[k] = left[iter1]
+			iter1 += 1
+			k += 1
+
+		while iter2 < len(right):
+			iter2 += 1
+			k += 1
+
+if __name__ == "__main__":
+	array = [4,3,6,2,2,1,10]
+	print(mergeSort(array))ß
