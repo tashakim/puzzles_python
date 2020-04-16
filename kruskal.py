22c9cde499312f6ad72f6a00ba87c38b4c601e5a
Tasha Kim <jae_young_kim@brown.edu>
2020-04-15 23:13:10 -0400

    April15

--- kruskal.py
+++ kruskal.py
@@ -2,4 +2,11 @@ def kruskal(g):
 	#Input: 
 	#Output:
 	for v in g.vertices():
-		
+		# make cloud for v
+	min_spanning_tree = []
+	# sort all edges here
+	for e in g.edges(): # edge between u and v
+		# if u and v not in same cloud:
+			# a add edge to min_spanning_tree
+			# merge clouds containing u and v
+	return min_spanning_tree
