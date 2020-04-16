cec83858905d911d757dc132458d1a249e7c2033
Tasha Kim <jae_young_kim@brown.edu>
2020-04-15 23:19:21 -0400

    April15

--- kruskal.py
+++ kruskal.py
@@ -12,4 +12,15 @@ def kruskal(g):
 		# if u and v not in same cloud:
 			# a add edge to min_spanning_tree
 			# merge clouds containing u and v
+
 	return min_spanning_tree
+
+def cloud(a,b):
+	A = find(a)
+	B = find(b)
+	parent[A] = B
+
+def find(A):
+	while parent[A] != A:
+		A = parent[A]
+	return A
