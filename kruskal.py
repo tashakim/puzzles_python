48220bce8ff28f39b7e444d447f686925683dd00
Tasha Kim <jae_young_kim@brown.edu>
2020-04-15 23:26:25 -0400

    April15

--- kruskal.py
+++ kruskal.py
@@ -1,17 +1,21 @@
 from mygraph import *
 from heappriorityqueue import *
 
+
 def kruskal(g):
 	#Input: 
 	#Output:
 	for v in g.vertices():
 		make_cloud(v)
 	min_spanning_tree = []
-	# sort all edges here
+	g.edges().sort()
 	for e in g.edges(): # edge between u and v
 		# if u and v not in same cloud:
+		if find(u) != find(v):
 			# a add edge to min_spanning_tree
+			minimum_spanning_tree.add(edge)
 			# merge clouds containing u and v
+			cloud(u,v)
 
 	return min_spanning_tree
 
@@ -19,12 +23,18 @@ def make_cloud(v):
 	parent[v] = v
 	rank[v] = 0
 
-def cloud(a,b):
+def cloud(u,v):
 	A = find(a)
 	B = find(b)
-	parent[A] = B
+	if A != B:
+		if rank[u]>rank[v]:
+			parent[v] = u
+		else:
+			parent[u] = v
+		if rank[u] == rank[v]:
+			rank[v] = rank[v]+1
 
-def find(A):
-	while parent[A] != A:
-		A = parent[A]
-	return A
+def find(v):
+	if parent[v] != v:
+		parent[v] = find(parent[v])
+	return parent[v]
