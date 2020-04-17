a34bd4fe3c2c8fc79e9318a715992e306bd72a6a
Tasha Kim <jae_young_kim@brown.edu>
2020-04-16 23:07:57 -0400

    April16

--- prim_jar.py
+++ prim_jar.py
@@ -1,4 +1,6 @@
 def prim(g):
 	# Purpose: Implementation of the prim-jarnik algorithm
-	# Input: weighted, undirected graph 
+	# Input: weighted, undirected graph g with vertices v
 	# Output: list of edges in the minimum spanning tree
+	for v in g.vertices():
+		
