3b3eb30568513573e94dc4020bb79879c01cb79f
Tasha Kim <jae_young_kim@brown.edu>
2020-04-16 23:16:04 -0400

    April16

--- prim_jar.py
+++ prim_jar.py
@@ -3,4 +3,21 @@ def prim(g):
 	# Input: weighted, undirected graph g with vertices v
 	# Output: list of edges in the minimum spanning tree
 	for v in g.vertices():
-		
+		v._cost = float('inf')
+		v._prev = None
+
+
+	#source = random vertex in g.vertices()
+	source._cost = 0
+	min_spanning_tree = []
+	# Q = priorityQueue(vertex)
+	while Q is not None:
+		vertex = Q.removeMin()
+		if vertex._prev != None:
+			min_spanning_tree.append((vertex, vertex._prev))
+		# for edges (vertex,u) of vertex such that u is in Q:
+			# if u._cost > edge (v,u)._weight:
+				# u._cost = edge (v,u)._weight
+				# u._prev = vertex
+				# Q.decreaseKey(u, u._cost)
+	return min_spanning_tree
