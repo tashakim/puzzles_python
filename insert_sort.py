6e4a1ca3f850da7344b75bc771389512263acf80
Tasha Kim <jae_young_kim@brown.edu>
2020-04-18 23:32:44 -0400

    April18

--- insert_sort.py
+++ insert_sort.py
@@ -1,3 +1,15 @@
 def insert_sort(array):
 	for i in range(1, len(array)):
-		
+		key = array[i]
+
+		j = i-1
+		while j>= and array[j] > key:
+			array[j+1] = array[j]
+			j -= 1
+
+		array[j+1] = key
+
+	return array
+
+if __name__ == "__main__":
+	
