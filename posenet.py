f6fb6027061092f65ace746ddd2f4fc8cf82d4c5
Tasha Kim <jae_young_kim@brown.edu>
2020-05-06 22:38:20 -0400

    posenet

--- posenet.py
+++ posenet.py
@@ -1,2 +1,11 @@
 <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
 <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/posenet"></script>
+
+
+const net = await posenet.load();
+
+const net = await posenet.load();
+
+const pose = await net.estimateSinglePose(image, {
+  flipHorizontal: false
+});
