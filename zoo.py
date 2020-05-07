a67a7886dd6a74c9e6841c7aad5d3b92f84dc269
Tasha Kim <jae_young_kim@brown.edu>
2020-05-06 23:32:25 -0400

    zoo

--- zoo.py
+++ zoo.py
@@ -4,17 +4,25 @@ class Zoo:
 		self.animal1 = animal1
 		self.animal2 = animal2
 		self.animal3 = animal3
+		print("\nCreated a new zoo with", self.number, "animals!")
 
 	def rename(self, animal1, newName):
 		self.animal1 = newName
+		print(self.animal1, "is now", newName)
 
 	def brush(self, animal):
 		print("Brushed", animal)
+
+	def feed(self, animal):
+		print("Fed", animal)
 
 if __name__ == "__main__":
 	zoo = Zoo(3, "zebra", "giraffe", "panther")
 	print(zoo.animal1)
 
 	zoo.rename("zebra", "my zebra")
-	print(zoo.animal1)
 	zoo.brush(zoo.animal1)
+	zoo.feed(zoo.animal2)
+
+
+	newzoo = Zoo(0, None, None, None)
