1a12ad7c7558190d6d871f98131a15a65d45839c
Tasha Kim <jae_young_kim@brown.edu>
2020-05-09 23:44:07 -0400

    deal with null string

--- bestCamelCase.py
+++ bestCamelCase.py
@@ -1,6 +1,6 @@
 def to_camel_case(text):
-    #your code here
+    # text[:1] instead of text[0] deals with null string
     return text[:1]+text.title()[1:].replace("-", "").replace("_", "")
 
-s="to_camel_case"
+s = "to_camel_case"
 print(to_camel_case(s))
