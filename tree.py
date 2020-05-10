5b3a7fa2d206829a8739eca943a6b89982726b2a
Tasha Kim <jae_young_kim@brown.edu>
2020-05-09 22:11:44 -0400

    giving tree

--- tree.py
+++ tree.py
@@ -6,8 +6,15 @@ class Tree:
 	def root(self):
 		return self._root
 
+	def addChild(self, child):
+		self._child = child
 
+	def child(self):
+		return self._child
 
 if __name__ == "__main__":
 	mytree = Tree(3)
 	print("Your root is: ", mytree.root())
+
+	mytree.addChild(1)
+	print("Your child is: ", mytree.child())
