d8a466b179b9dc2f86e9c551325c235672f93498
Tasha Kim <jae_young_kim@brown.edu>
2020-05-09 22:10:32 -0400

    giving tree

--- tree.py
+++ tree.py
@@ -1,11 +1,13 @@
 class Tree:
 	def __init__(self, root):
 		self._root = root
+		print("new  giving tree created")
 
 	def root(self):
 		return self._root
 
 
+
 if __name__ == "__main__":
 	mytree = Tree(3)
-	print(mytree.root())
+	print("Your root is: ", mytree.root())
