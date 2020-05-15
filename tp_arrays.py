a3fda55346b6a61a57e96ecbc5cd399544ea221d
Tasha Kim <jae_young_kim@brown.edu>
2020-05-14 23:19:28 -0400

    May14

--- tp_arrays.py
+++ tp_arrays.py
@@ -1,9 +1,12 @@
 from array import *
 
 array1 = array('i', [10,20,30,40,50])
+array2 = array('d', [0.1,0.2,0.3,0.4,0.5])
 
 for x in array1:
 	print(x)
+for y in array2:
+	print(y)
 
 
 list1 = [10,20,30,40,50]
