ef71f474eb2d75965328fa61089c26bb9968d19b
Tasha Kim <jae_young_kim@brown.edu>
2020-05-14 23:22:47 -0400

    May14

--- tp_arrays.py
+++ tp_arrays.py
@@ -1,5 +1,7 @@
 from array import *
 
+# list vs. array
+
 array1 = array('i', [10,20,30,40,50])
 array2 = array('l', [10,20,30,40,50])
 array3 = array('d', [0.1,0.2,0.3,0.4,0.5])
@@ -9,9 +11,9 @@ for x in array1:
 for y in array2:
 	print(y)
 for i in range(5):
-	print(array3[i], end = " ")
+	print(array3[i], end = " ") # print on same line
 
 list1 = [10,20,30,40,50]
 
 for x in list1:
-	print(x)
+	print(x) 
