add4be73b1c179c175f5f0d702bcc5b928367710
Tasha Kim <jae_young_kim@brown.edu>
2020-05-20 23:35:07 -0400

    May20

--- dynamic_fib.py
+++ dynamic_fib.py
@@ -1,14 +1,15 @@
 def dynamic_fibonacci(n) :
 
 	fib = []
-	fib.append(0)
-	fib.append(1)
-
-	print(fib)
+	#fib.append(0)
+	#fib.append(1)
+	fib.extend([0,1])
+	print("fib[0] is: ", fib[0])
+	print("fib[1] is: ", fib[1])
 
 	for i in range(2,n+1) :
 		fib.insert(i, fib[i-1 + fib[i-2]])
-
+		print("fib[",i,"] is: ", fib[i])
 	return fib[n]
 
 for i in range(0,10):
