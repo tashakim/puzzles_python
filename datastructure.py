6d3962cf36c1c5bef3e7411b4ce0333e581e6fc0
Tasha Kim <jae_young_kim@brown.edu>
2020-05-21 23:33:20 -0400

    May21

--- datastructure.py
+++ datastructure.py
@@ -8,7 +8,13 @@ def add_to_structure(x):
 	mylist.append(x)
 	myqueue.append(x)
 
-def remove_from_structure(x):
+def remove_from_structure():
 	mylist.pop()
 	myqueue.pop(0)
-	
+
+
+if __name__ == "__main__":
+	initialize()
+
+	add_to_structure(1)
+	remove_from_structure()
