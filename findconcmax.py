923140d48c815221a00ddcb0b2082f77831c7557
Tasha Kim <jae_young_kim@brown.edu>
2020-06-04 22:15:31 +0900

    Jun4

--- findconcmax.py
+++ findconcmax.py
@@ -5,19 +5,25 @@ def findConsecutiveMax(arr):
   max_count = 1
   count = 1
   cur = arr[0]
+  char = cur
 
   for i in range(1,len(arr)):
     if(arr[i] == cur):
       count +=1
       if(count > max_count):
         max_count = count
+        char = arr[i]
+
     else:
       count = 1
     cur = arr[i]
-  return max_count
+
+  return char
 
 if __name__ == "__main__":
+
   s=['c','c','a','b','e','e','e']
-  print(findConsecutiveMax(s))
+  print(findConsecutiveMax(s)) # 'e' occurs 3 times
+
   t= ['e','f','g','a','a','e','a','b','f']
-  print(findConsecutiveMax(t)) 
+  print(findConsecutiveMax(t)) # 'a' occurs 2 times
