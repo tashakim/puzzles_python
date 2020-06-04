b8739b7fc41615be8929e225aa872a54278412a4
Tasha Kim <jae_young_kim@brown.edu>
2020-06-04 23:53:03 +0900

    num max

---
+++ nummaxword.py
@@ -0,0 +1,10 @@
+def max_word(s):
+	"""Purpose: takes in a string, and returns the no. of occurrences of most common word in string.
+	Example: max_word("Hello world!") -> 1
+	max_word("the quick the fox") -> 2
+	"""
+	list(s)
+
+if __name__ == "__main__":
+	print(max_word("Hello world!"))
+	print(max_word("the quick the fox"))
