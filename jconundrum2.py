45f9c4e4c9c619c8a4fb0b3a2f4223588f540519
Tasha Kim <jae_young_kim@brown.edu>
2020-06-05 02:32:50 +0900

    clean sol

--- jconundrum2.py
+++ jconundrum2.py
@@ -2,6 +2,9 @@ def conundrum(arr):
 	"""Purpose: returns maximum profits given an array of profits on each day.
 	Example: conundrum([-1,2,7,-8,13,-2]) -> 194
 	"""
+	if(arr == []):
+		return 180
+
 	max_profit = -float("inf")
 	cur_max = 0
 
@@ -14,5 +17,9 @@ def conundrum(arr):
 			cur_max = 0
 	return max_profit + 180
 
+
 if __name__ == "__main__":
-	print(conundrum([-1,2,7,-8,13,-2]))
+	assert(conundrum([-1,2,7,-8,13,-2]) == 194), "Wrong value"
+	assert(conundrum([]) == 180), "Wrong value"
+
+	print("All tests passed!")
