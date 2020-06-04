342a56b56e7aa8f912b4dc46723079393c0a2e3c
Tasha Kim <jae_young_kim@brown.edu>
2020-06-04 23:45:18 +0900

    arrsearch

--- myarrsearch.py
+++ myarrsearch.py
@@ -7,6 +7,13 @@ def arraysearch(num, arr):
 			return True
 	return False
 
+def simplearrsearch(num, arr):
+	"""Purpose: simplifies the implementation of the above function.
+	"""
+	return num in arr
+
+
 if __name__ == "__main__":
-	print(arraysearch(2, [1,2,3]))
-	print(arraysearch(2, [1,3,4]))
+
+	print(arraysearch(2, [1,2,3]) == simplearrsearch(2, [1,2,3]))
+	print(arraysearch(2, [1,3,4]) == simplearrsearch(2, [1,3,4]))
