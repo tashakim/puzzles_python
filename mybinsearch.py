b69bf746e605122960a730e510694fb11fcc19f5
Tasha Kim <jae_young_kim@brown.edu>
2020-06-06 17:15:33 +0900

    binsearch imp

---
+++ mybinsearch.py
@@ -0,0 +1,27 @@
+def binarysearch(arr, x):
+	"""Purpose: Python implementation of binary search.
+	"""
+	if(len(arr) == 0):
+		return False
+
+	if(len(arr) == 1):
+		return arr[0] == x
+
+	mid = len(arr)//2
+
+	if(x ==  arr[mid]):
+		return True
+	if(x > arr[mid]):
+		return binarysearch(arr[mid+1:],x)
+
+	if(x < arr[mid]):
+		return binarysearch(arr[:mid],x)
+
+
+if __name__ == "__main__":
+	a = [1,2,3,4,5]
+	assert(binarysearch(a, 3) == True), "Wrong answer"
+	assert(binarysearch(a, 5) == True), "Wrong answer"
+	assert(binarysearch(a, 6) == False), "Wrong answer"
+
+	print("All tests passed!")
