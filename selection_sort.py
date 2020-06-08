39237c278a3bf38d2a7f97e40b375a962157b7ed
Tasha Kim <jae_young_kim@brown.edu>
2020-06-08 20:36:28 +0900

    selection sort imp

--- selection_sort.py
+++ selection_sort.py
@@ -2,7 +2,7 @@ def selection_sort(arr):
 	"""Purpose: Python implementation of selection sort.
 	"""
 	n = len(arr)
-	for i range(n-2):
+	for i in range(n-2):
 		min = argmin(arr[i: n-1])
 		arr[i] = arr[min]
 	return arr
