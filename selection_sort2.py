180e1840c241c0d09ada7f3fa366e63bef376c7c
Tasha Kim <jae_young_kim@brown.edu>
2020-06-08 21:35:11 +0900

    selection sort

--- selection_sort2.py
+++ selection_sort2.py
@@ -1,16 +1,24 @@
+class InvalidInputException:
+	pass
+
 def selection_sort2(arr):
 	"""Purpose: Simple python implementation of selection sorting algorithm.
 	"""
+	if(arr == None or arr == []):
+		raise InvalidInputException("Array is invalid.")
+
 	for i in range(len(arr)):
 		min_index = i
 		for j in range(i+1, len(arr)):
 			if(arr[min_index] > arr[j]):
 				min_index = j
 		arr[i], arr[min_index] =  arr[min_index], arr[i]
+	
 	return arr
 
 
 if __name__ == "__main__":
 	a = [5,3,2,4,1]
+	b = [23,41,50,0,3]
 	assert(selection_sort2(a) == [1,2,3,4,5]), "Wrong answer"
 	print("All tests passed!")
