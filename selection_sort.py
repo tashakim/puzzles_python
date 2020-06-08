16257cd450d3a812fa9131c060bb701ad12b6729
Tasha Kim <jae_young_kim@brown.edu>
2020-06-08 20:36:00 +0900

    selection sort implementation

--- selection_sort.py
+++ selection_sort.py
@@ -4,4 +4,9 @@ def selection_sort(arr):
 	n = len(arr)
 	for i range(n-2):
 		min = argmin(arr[i: n-1])
-		arr(i) = arr(min)
+		arr[i] = arr[min]
+	return arr
+
+if __name__ == "__main__":
+	a = [4,5,1,2,3,6]
+	selection_sort(a)
