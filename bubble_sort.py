4205f0e69eb2f65bc6adc8f64be23354d645bb82
Tasha Kim <jae_young_kim@brown.edu>
2020-06-08 23:02:00 +0900

    bubble_sort imp

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,29 @@
+class InvalidInputException:
+	"""Purpose: Exception will be thrown when input array is
+	invalid.
+	"""
+	pass
+
 def bubbleSort(arr):
 	"""Purpose: Python implementation of a bubble sorting algorithm.
 	"""
-	for i in range(len(arr)):
-		for j in range(len(arr)-i):
+	if(arr == None or arr == []):
+		raise InvalidInputException("Input is invalid")
+
+	for i in range(len(arr) -1):
+		for j in range(len(arr)-i -1):
 			if(arr[j] > arr[j+1]):
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 
 	return arr
 
+
 if __name__ == "__main__":
 	a = [32,30,0,1,2]
-	print(bubbleSort(a))
+	a2 = [0,0,0,1,0]
+	a3 = [-1,4,-5,9,11]
+	assert(bubbleSort(a) == [0,1,2,30,32]), "Wrong order"
+	assert(bubbleSort(a2) == [0,0,0,0,1]), "Wrong order"
+	assert(bubbleSort(a3) == [-5,-1,4,9,11]), "Wrong order"
+
+	print("All tests passed!") 
