be1a4484b92a599bc0b9fd0f6820fae50ef30e84
Tasha Kim <jae_young_kim@brown.edu>
2020-06-09 22:11:36 +0900

    divisorgame

---
+++ divisorGame.py
@@ -0,0 +1,19 @@
+def divisorGame(n):
+	"""Purpose: Alice goes first, then Bob. 
+	Choose any x s.t. 0 < x < n, and n%x ==0.
+	Then replace n with n-x.
+	If only Alice wins, return True. Else False.
+	"""
+	if(n%2 ==0):
+		return True
+	return False
+
+if __name__ == "__main__":
+	for i in range(1, 10):	
+		print("n is: ", i, "and result is: ,", divisorGame(i))
+		if(divisorGame(i) == True):
+			divisors = []
+			for div in range(1,i):
+				if(i%div == 0):
+					divisors.append(div)
+			print("div = ", divisors)
