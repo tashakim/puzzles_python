1565e7f0b7f46ffe0c201abe88a62722bf526c32
Tasha Kim <jae_young_kim@brown.edu>
2020-06-09 22:39:45 +0900

    unique paths

--- uniquePaths.py
+++ uniquePaths.py
@@ -1,4 +1,8 @@
-def uniquePaths(self, m: int, n: int) -> int:
+def uniquePaths(m: int, n: int) -> int:
+	"""Purpose: A robot located at the top-left corner
+	of a mxn grid can either move down or right.
+	How many unique paths are there?
+	"""
         temp = [[0 for i in range(m)] for i in range(n)]
         for i in range(n):
             for j in range(m):
@@ -7,4 +11,10 @@ def uniquePaths(self, m: int, n: int) -> int:
                 else:
                     temp[i][j] = temp[i-1][j] + temp[i][j-1]
         return temp[n-1][m-1]
-        
+
+if __name__ == "__main__":
+	assert(uniquePaths(1,1) == 1), "Wrong  answer "
+	assert(uniquePaths(2,2) == 2), "Wrong answer"
+	assert(uniquePaths(9,3) == 45), "Wrong answer"
+
+	print("All tests passed!")
