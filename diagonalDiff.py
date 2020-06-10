07b3a1b1355041eebecd36f25648925989d76d77
Tasha Kim <jae_young_kim@brown.edu>
2020-06-10 23:18:36 +0900

    diagonals

--- diagonalDiff.py
+++ diagonalDiff.py
@@ -1,14 +1,23 @@
+class InvalidInputException:
+	pass
+
 def diagonalDiff(arr):
-	"""Purpose:
+	"""Purpose: Calculates the absolute difference
+	between sums of the diagonals, of a square matrix.
 	"""
-	sum1 = 0
-	sum2 = 0
+	if(len(arr) != len(arr[0])):
+		raise InvalidInputException("Input is invalid.")
+
+	sum1, sum2 = 0, 0
 	for i in range(len(arr)):
 		sum1 += arr[i][i]
 	for i in range(len(arr)):
 		sum2 += arr[i][-i-1]
 	return abs(sum1 - sum2)
 
+
 if __name__ == "__main__":
-	a = [[1,2,3], [4,5,6], [7,8,9]]
-	diagonalDiff(a)
+	a = [[11,2,4], [4,5,6], [10,8,-12]]
+	assert(diagonalDiff(a) == 15), "Wrong answer"
+
+	print("Test passed!")
