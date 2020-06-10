243fa482a466d5bbac7d10ce011eedf2c4bfad62
Tasha Kim <jae_young_kim@brown.edu>
2020-06-10 22:30:06 +0900

    largesum

--- largesum.py
+++ largesum.py
@@ -7,14 +7,17 @@ import re
 import sys
 
 # Complete the aVeryBigSum function below.
-def aVeryBigSum(ar):
+def aVeryBigSum(ar): # passes
     return sum(ar)
 
-def secondVersion(ar):
+def secondVersion(ar): # passes
 	result = 0 
 	for i in range(len(ar)):
 		result += arr[i]
+
 	return result
+
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
