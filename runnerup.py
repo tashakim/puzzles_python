22742d8aad36072413ba72685f6eeee0212cf8f6
Tasha Kim <jae_young_kim@brown.edu>
2020-06-10 16:04:59 +0900

    runnerup

--- runnerup.py
+++ runnerup.py
@@ -1,10 +1,21 @@
-def runnerup(n, arr):
+def runnerupScore(n, arr):
 	"""Purpose: Given an array of n scores, 
 	returns the runner-up score.
 	"""
 	arr = [x for x in arr if x!=max(arr)]
-	print(max(arr))
-	
+
+	return max(arr)
+
+
+def bestScore(n,arr):
+	"""Purpose: Returns the best score from input array.
+	"""
+	return max(arr)
 
 if __name__ == "__main__":
-	runnerup(5,[2,3,6,6,5])
+	runnerupScore(5,[2,3,6,6,0])
+
+	assert(bestScore(5,[2,3,6,6,5]) == 6), "Wrong answer"
+	assert(runnerupScore(5,[2,3,6,6,5]) == 5), "Wrong answer"
+	print("All tests passed!")
+
