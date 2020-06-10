8c832f21739b6c7f641d4c50c4f29169ba447d8f
Tasha Kim <jae_young_kim@brown.edu>
2020-06-10 15:29:59 +0900

    two-d listcomp

--- 2dlist_comp.py
+++ 2dlist_comp.py
@@ -1,4 +1,4 @@
-def twoDlist_comp(x,y, N):
+def tdlist_comp(x,y, N):
 	"""Purpose: prints out all coordinates [x,y]
 	such that x+y does not equal N.
 	"""
@@ -11,6 +11,26 @@ def twoDlist_comp(x,y, N):
 	print(coordinates)
 
 
+def thdlist_comp(x,y,z,N):
+	"""Purpose: prints out all coordinates [x,y,z] 
+	such that x+y+z   does not equal N.
+	"""
+	coordinates = []
+	for i in range(x+1):
+		for j in range(y+1):
+			for k in range(z+1):
+				if(i+j+k!=N):
+					coordinates.append([i,j,k])
+	print(coordinates)
+
+
 if __name__ == "__main__":
-	twoDlist_comp(1,1,2)
-	twoDlist_comp(2,3,4)
+	print("two-dim list comp: \n")
+	tdlist_comp(1,1,2)
+	tdlist_comp(2,3,4)
+	print()
+
+	print("three-dim list comp: \n")
+	thdlist_comp(1,1,1,2)
+	thdlist_comp(2,2,2,4)
+	print()
