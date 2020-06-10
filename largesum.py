0bfd9a42d60ef46f170fda321721b5d7afd36e46
Tasha Kim <jae_young_kim@brown.edu>
2020-06-10 22:29:46 +0900

    largesum

--- largesum.py
+++ largesum.py
@@ -11,6 +11,10 @@ def aVeryBigSum(ar):
     return sum(ar)
 
 def secondVersion(ar):
+	result = 0 
+	for i in range(len(ar)):
+		result += arr[i]
+	return result
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
