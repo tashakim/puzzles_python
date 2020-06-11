4c62815bbabba4787056be50ed8ebdb116cf87b4
Tasha Kim <jae_young_kim@brown.edu>
2020-06-11 23:55:19 +0900

    merge sort imp

--- merge_sort.py
+++ merge_sort.py
@@ -1,9 +1,10 @@
 def mergesort(arr, l, r):
-	"""Purpose:
+	"""Purpose: Simple python implementation of merge sort
+	algorithm.
 	"""
 	mid = (l+r)//2
 	mergesort(arr, l, m)
 	mergesort(arr, m+1, r)
 
+
 if __name__ == "__main__":
-	
