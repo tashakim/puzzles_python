6963e484e5f76bd824de5e610c8ac1495639416a
Tasha Kim <jae_young_kim@brown.edu>
2020-06-11 22:26:46 +0900

    hourglass sum

--- hourglassSum.py
+++ hourglassSum.py
@@ -2,9 +2,10 @@ def hourglass(arr):
 	"""Purpose:
 	"""
 	sum = []
-	for i in range(len(arr)-2):
+	for i in range(len(arr[0])-2):
 		for j in range(len(arr)-2):
-			sum.append(arr[i][j] + arr[i+1][j] + arr[i+2][j] + arr[i+1][j+1] +arr[i][j+2] + arr[i+1][j+2] + arr[i+2][j+2])
+			# sum.append(arr[i][j] + arr[i+1][j] + arr[i+2][j] + arr[i+1][j+1] +arr[i][j+2] + arr[i+1][j+2] + arr[i+2][j+2])
+			sum.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
 	print(max(sum))
 
 if __name__ == "__main__":
