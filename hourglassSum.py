f29544858503134382aeb84308757a5f56057345
Tasha Kim <jae_young_kim@brown.edu>
2020-06-11 22:29:07 +0900

    hourglass sum

--- hourglassSum.py
+++ hourglassSum.py
@@ -1,13 +1,22 @@
+class InvalidArrayException:
+	pass
+
 def hourglass(arr):
-	"""Purpose:
+	"""Purpose: Prints the maximum sum of hourglass 
+	values.
 	"""
+	if(len(arr) < 0 or len(arr[0]) < 0):
+		raise InvalidArrayException("Input array is invalid")
+
 	sum = []
 	for i in range(len(arr[0])-2):
 		for j in range(len(arr)-2):
 			# sum.append(arr[i][j] + arr[i+1][j] + arr[i+2][j] + arr[i+1][j+1] +arr[i][j+2] + arr[i+1][j+2] + arr[i+2][j+2])
 			sum.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
-	print(max(sum))
+	return max(sum)
+
 
 if __name__ == "__main__":
 	arr = [[1,1,1,0,0,0], [0,1,0,0,0,0],[1,1,1,0,0,0],[0,0,2,4,4,0],[0,0,0,2,0,0],[0,0,1,2,4,0]]
-	hourglass(arr)
+	assert(hourglass(arr) == 19), "Wrong answer"
+	print("Test passed!")
