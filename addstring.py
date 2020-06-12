24807837cd4e48eb1d4ddd2b95df1c3077d75d65
Tasha Kim <jae_young_kim@brown.edu>
2020-06-12 23:50:08 +0900

    adddstrings

--- addstring.py
+++ addstring.py
@@ -5,8 +5,9 @@ class Solution:
 
     def addStrings(self, num1: str, num2: str) -> str:
         if(!isinstance(num1, str) or !isinstance(num2, str)):
-        	
-        return str(int(num1) + int(num2))
+        	raise InvalidInputException("Input is invalid.")
+       
+       return str(int(num1) + int(num2))
 
 
 if __name__ == "__main__":
