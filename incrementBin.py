ee2c3c4e07c5040c9422574df311f865c2f35a09
Tasha Kim <jae_young_kim@brown.edu>
2020-06-13 21:46:17 +0900

    increment bin

--- incrementBin.py
+++ incrementBin.py
@@ -1,8 +1,18 @@
-def increment(n):
-	"""Purpose: Takes in a stack of 0s and 1s, respresenting a binary number k, and returns a stack representing binary number k+1.
+def increment(k):
+	"""Purpose: Takes in a stack of 0s and 1s, respresenting 
+	a binary number k, and returns a stack representing 
+	binary number k+1.
 	"""
+	if(k[-1] == 1):
+		k.pop()
+
+
 
 if __name__ == "__main__":
+	print(increment([1]))
+
 	assert(increment([1,0,0,0]) == [1,0,0,1]), "Wrong answer."
 	assert(increment([1]) == [1,0]), "Wrong answer."
 	assert(increment([0,0,1]) == [0,1,0]), "Wrong answer."
+	
+	print("All tests passed!")
