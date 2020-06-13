882551396a9c2468e7a197ebaf0303de73ba8d90
Tasha Kim <jae_young_kim@brown.edu>
2020-06-13 21:48:04 +0900

    increment bin

--- incrementBin.py
+++ incrementBin.py
@@ -1,12 +1,18 @@
+class InvalidInputException:
+	pass
+
 def increment(k):
 	"""Purpose: Takes in a stack of 0s and 1s, respresenting 
 	a binary number k, and returns a stack representing 
 	binary number k+1.
 	"""
+	if(k == [] or k == None):
+		raise InvalidInputException("Input must be valid.")
+
 	if(k[-1] == 1):
 		k.pop()
 		return increment(k[:-1])
-		
+
 	elif(k[-1] == 0):
 		k[-1] +=1
 
