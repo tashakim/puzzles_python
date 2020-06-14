e71b52adcf64ecfc2da43fb7e25d6710bf15f46f
Tasha Kim <jae_young_kim@brown.edu>
2020-06-14 23:02:29 +0900

    recipes

--- cakerecipe.py
+++ cakerecipe.py
@@ -14,14 +14,22 @@ def cakerecipe(recipe, available):
 
 	return min_n
 
+def better_sol(recipe, available):
+	return min(available.get(item,0)//recipe[item] for item in recipe)
+	# get function in python has two (2) parameters:
+	# first param: key. second param: value returned when not found.
+
 
 if __name__ == "__main__":
 	recipe = {"flour": 500, "sugar": 200, "eggs": 1}
 	available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
 	assert(cakerecipe(recipe, available) == 2), "Wrong answer."
+	assert(cakerecipe(recipe, available) == better_sol(recipe, available)), "Wrong answer."
+
 
 	recipe = {"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}
 	available = {"sugar": 500, "flour": 2000, "milk": 2000}
 	assert(cakerecipe(recipe, available) == 0), "Wrong answer."
+	assert(cakerecipe(recipe, available) == better_sol(recipe, available)), "Wrong answer."
 
 	print("\nAll tests passed!")
