eaf6b0533826aaf6872a5869d4dfaac2fcd68654
Tasha Kim <jae_young_kim@brown.edu>
2020-06-17 23:49:42 +0900

    sortstack

--- sortstack.py
+++ sortstack.py
@@ -4,6 +4,8 @@ class EmptyStackException(Exception):
 class SortStack:
 	"""Purpose: Sorts a stack such that the smallest items
 	are on the top.
+	Idea 1: hint: sort elements within stack.
+	Idea 2 hint: use three stacks, two of which are temporary.
 	"""
 	def __init__(self, cap):
 		self._data = [None]*cap
@@ -20,6 +22,7 @@ class SortStack:
 		# sorts all items after appending.
 		return
 
+
 	def pop(self):
 		if(self._size == 0):
 			raise EmptyStackException("Check again. Stack is empty.")
@@ -29,6 +32,7 @@ class SortStack:
 			self._top -=1
 		# sorts remaining items after removing.
 		return
+
 
 	def peek(self):
 		return self._data[self._top]
@@ -44,3 +48,4 @@ class SortStack:
 
 if __name__ == "__main__":
 	s= SortStack()
+
