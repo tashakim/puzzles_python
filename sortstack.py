d03470201150f008e6d1e45cf44b158bb6187317
Tasha Kim <jae_young_kim@brown.edu>
2020-06-17 23:40:15 +0900

    sortstack

--- sortstack.py
+++ sortstack.py
@@ -1,5 +1,16 @@
 class SortStack:
+	"""Purpose: Sorts a stack such that the smallest items
+	are on the top, 
+	"""
 	def __init__(self):
+		return
+
+	def push(item):
+		return
+	def pop():
+		return
+
+	def printStack():
 		return
 
 if __name__ == "__main__":
