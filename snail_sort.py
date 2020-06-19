31fd1a2a71297d811598f5870ea2c35c65e3b401
Tasha Kim <jae_young_kim@brown.edu>
2020-06-19 10:56:58 +0900

    snailsort

--- snail_sort.py
+++ snail_sort.py
@@ -4,10 +4,11 @@ def snail(snail_map):
     order = []
     n = len(snail_map) # nxn map
     w, h = n, n
-    
+    # while h not 0
     for i in range(0,w):
         if(snail_map[0][i] not in order):
             order.append(snail_map[0][i])
+            # decrement h
     for j in range(1,h):
         if(snail_map[j][n-1] not in order):
             order.append(snail_map[j][n-1])
@@ -16,3 +17,11 @@ def snail(snail_map):
             order.append(snail_map[n-1][k])
     #for l in range(3):
     return order
+    
+
+if __name__ == "__main__":
+    arr = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+    assert(snail(arr) == [1,2,3,6,9,8,7,4,5]), "Wrong answer."
+    print("Test passed.")
