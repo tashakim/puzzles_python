daf36495f8e457395d7146dc692fce83e644cbd1
Tasha Kim <jae_young_kim@brown.edu>
2020-06-20 17:50:38 +0900

    convert base

---
+++ convertBase.py
@@ -0,0 +1,10 @@
+def compareNumbers(bin, hex):
+	"""Purpose: Code reuse, to check if value of a 
+	binary number (passed in as string) equals the 
+	hexadecimal representation of a string.
+	"""
+
+
+def convert(n, base):
+	"""Purpose: Converts the number written in base 'base'.
+	"""
