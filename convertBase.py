cd487809e9f47ef817c12e6dbec9fb7b9cd92625
Tasha Kim <jae_young_kim@brown.edu>
2020-06-20 21:42:59 +0900

    convert base

--- convertBase.py
+++ convertBase.py
@@ -3,8 +3,20 @@ def compareNumbers(bin, hex):
 	binary number (passed in as string) equals the 
 	hexadecimal representation of a string.
 	"""
+	if (convert(bin, 2) <0 or convert(hex, 16) <0):
+		return False
+	return True
 
 
 def convert(n, base):
 	"""Purpose: Converts the number written in base 'base'.
 	"""
+	for i in range(len(n)):
+		n = int(n)
+		if(n<0 || n >= base):
+			return -1
+		exp = len(n) -1 -i
+		val += n*Math.pow(base, exp)
+	return val
+
+
