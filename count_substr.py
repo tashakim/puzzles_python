5455103eefeeb3de85c77e662652a459dc1b5db1
Tasha Kim <jae_young_kim@brown.edu>
2020-06-22 01:19:29 +0900

    count substr

--- count_substr.py
+++ count_substr.py
@@ -5,7 +5,16 @@ def count_substr(s,c):
 	return sum([1 for i in range(len(s) - len(c)+1) if s[i:i+len(c)] == c])
 
 
+def unpacked(s,c):
+	count = 0
+	for i in range(len(s)-len(c)+1):
+		if(s[i:i+len(c)] == c):
+			count += 1
+	return count
+
+
 if __name__ == "__main__":
 	s = "cabacab"
 	c = "cab"
 	print(count_substr(s,c))
+	print(unpacked(s,c))
