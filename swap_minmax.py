a42ab16f66e7dd22a30d18fc912e9b992309fd05
Tasha Kim <jae_young_kim@brown.edu>
2020-06-21 16:19:32 +0900

    swapping

--- swap_minmax.py
+++ swap_minmax.py
@@ -3,25 +3,42 @@ def swapMinMax(arr):
 	method, (2) implemented in a more modular way, by 
 	separating into two different methods.
 	"""
-	minIndex = 0
+	minIndex, maxIndex = 0, 0
 	for i in range(len(arr)):
 		if(arr[i] < arr[minIndex]):
 			minIndex = i
 
-	maxIndex = 0
 	for i in range(len(arr)):
 		if(arr[i] > arr[maxIndex]):
 			maxIndex = i
 
-	temp = arr[minIndex]
-	arr[minIndex] = arr[maxIndex]
-	arr[maxIndex] = temp
+	arr[minIndex], arr[maxIndex] = arr[maxIndex], arr[minIndex]
 	return arr
 
+
 def modular_sol(arr):
-	return
+	minIndex = getMinIndex(arr)
+	maxIndex = getMaxIndex(arr)
+	swapIndices(arr, minIndex, maxIndex)
+	return arr
+
+def getMinIndex(arr):
+	return arr.index(min(arr))
+
+def getMaxIndex(arr):
+	return arr.index(max(arr))
+
+def swapIndices(arr, minIndex, maxIndex):
+	arr[minIndex], arr[maxIndex] = arr[maxIndex], arr[minIndex]
+	return arr
+
 
 if __name__ == "__main__":
 	arr = [1,2,3,4,5]
 	assert(swapMinMax(arr) == [5,2,3,4,1]), "Wrong answer"
 	print("Test passed!")
+
+	assert(modular_sol(arr) == swapMinMax(arr)), "Wrong answer"
+	print("Second test passed!\n")
+
+	print("All tests passed!")
