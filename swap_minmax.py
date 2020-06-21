bf9a47aa8df741468f621583fbf0adeed0bcbeb5
Tasha Kim <jae_young_kim@brown.edu>
2020-06-21 16:29:47 +0900

    swapping

--- swap_minmax.py
+++ swap_minmax.py
@@ -1,8 +1,16 @@
+import pytest
+
+class InvalidInputException(Exception):
+	pass
+
 def swapMinMax(arr):
 	"""Purpose: Design (1) swapping min and max indices in one
 	method, (2) implemented in a more modular way, by 
 	separating into two different methods.
 	"""
+	if(arr is None or len(arr) < 2):
+		raise InvalidInputException("Input is invalid.")
+
 	minIndex, maxIndex = 0, 0
 	for i in range(len(arr)):
 		if(arr[i] < arr[minIndex]):
@@ -17,6 +25,9 @@ def swapMinMax(arr):
 
 
 def modular_sol(arr):
+	if(arr is None or len(arr) < 2):
+		raise InvalidInputException("Input is invalid.")
+
 	minIndex = getMinIndex(arr)
 	maxIndex = getMaxIndex(arr)
 	swapIndices(arr, minIndex, maxIndex)
@@ -32,6 +43,16 @@ def swapIndices(arr, minIndex, maxIndex):
 	arr[minIndex], arr[maxIndex] = arr[maxIndex], arr[minIndex]
 	return arr
 
+def testValidInput():
+	with pytest.raises(InvalidInputException):
+		swapMinMax([])
+		swapMinMax(None)
+		swapMinMax([0])
+
+		modular_sol([])
+		modular_sol(None)
+		modular_sol([3])
+
 
 if __name__ == "__main__":
 	arr = [1,2,3,4,5]
@@ -40,5 +61,6 @@ if __name__ == "__main__":
 
 	assert(modular_sol(arr) == swapMinMax(arr)), "Wrong answer"
 	print("Second test passed!\n")
+	testValidInput()
 
 	print("All tests passed!")
