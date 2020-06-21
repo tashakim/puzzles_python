8e52bf6ebcda5c9eb3b2c170773f877447f815a6
Tasha Kim <jae_young_kim@brown.edu>
2020-06-21 19:39:30 +0900

    string builder

--- stringbuilder.py
+++ stringbuilder.py
@@ -1,14 +1,27 @@
-def stringBuilder(my_list):
+from array import array
+
+def stringBuilder(strings):
 	"""Purpose: Concatenates a list of strings.
-	Method 1: Runtime complexity of O(xn^2),
-	Method 2: Runtime complexity of O(n)
+
+	# Method 1: Naive appending. 
+	Each time word is iterated, new string is created.
+	Runtime: O(xn^2).
 	"""
 	result = ""
-	for word in my_list:
-		result = result + word
+	for word in strings:
+		result += word
 	return result
+
+	# Method 2: Use character arrays. 
+	
+
+def stringlistBuilder(strings):
+	# Method 3: Build list of strings -> join into string once.
+	# Runtime: O(n).
+	return "".join(strings)
 
 
 if __name__ == "__main__":
 	l = ["result", "is", "a", "string"]
 	print(stringBuilder(l))
+	print(stringlistBuilder(l))
