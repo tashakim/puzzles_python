bd7e401c8a601c95a5277615c3734eb6869bb9e1
Tasha Kim <jae_young_kim@brown.edu>
2020-06-22 22:39:28 +0900

    hash

--- hashtable_imp.py
+++ hashtable_imp.py
@@ -12,9 +12,15 @@ class HashTable:
 		self._hashtable_size = n
 
 	def __getitem__(self, key):
+		if key not in self._table:
+			raise InvalidKeyException("Key is invalid. Try again.")
 
 	def __len__(self):
 		return #sum(len(sublist) for sublist in self._table)
+
+	def __contains__(self, key):
+		i = self._index(key)
+		return any(dict_key == key for dict_key in self._table[i])
 
 	def hash(self, key):
 		"""Purpose: Returns the index of the table, 
@@ -29,13 +35,16 @@ class HashTable:
 		if(self[key] is not None):
 			return self[key]
 		else:
-			raise InvalidKeyException("Key is invalid.")
+			raise InvalidKeyException("Key is invalid. Try again.")
+
 
 	def insert(self, key, value):
 		"""Purpose: Appends a value to the hash table,
 		by its key.
 		"""
-		return 
+		i = self._index(key)
+		self._table[i].append((key, value))
+		 
 
 	def find(self, key):
 		"""Purpose: Gets a value in hash table by key.
@@ -46,4 +55,5 @@ class HashTable:
 		"""Purpose: Removes a value from the hash table.
 		"""
 		return
+		
 
