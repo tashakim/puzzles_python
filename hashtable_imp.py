b73c0c3646e27a43873b1e5ecbb80a680de6733f
Tasha Kim <jae_young_kim@brown.edu>
2020-06-22 22:16:56 +0900

    hash

--- hashtable_imp.py
+++ hashtable_imp.py
@@ -1,8 +1,21 @@
 class HashTable:
 	def __init__(self, n = 5)
 		self._table = [None]*n
+		self._length = len(self._table)
 
 	def hash(self, key):
 		"""Purpose: Returns the index of the table, 
 		for a specific string 'key'.
 		"""
+		return hash(key) % self._length
+
+	def append(self, key, value):
+		"""Purpose: Appends a value to our table,
+		by its key.
+		"""
+		return 
+
+	def get(self, key):
+		"""Purpose: Gets a value by the key.
+		"""
+		return
