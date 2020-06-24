0e984da413f664497fa4a1728c637dd7eff10efc
Tasha Kim <jae_young_kim@brown.edu>
2020-06-24 15:09:26 +0900

    largest prime

--- largest_prime.py
+++ largest_prime.py
@@ -1,13 +1,34 @@
 def largest_prime_factor(n):
-    i = 2
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            n //= i
-    return n
+	"""Purpose: Returns the largest prime factor of 'n'.
+	"""
+	i = 2
+	while i * i <= n:
+		if n % i:
+			i += 1
+		else:
+			n //= i
+	return n
+	
+
+def all_prime_factors(n):
+	"""Purpose: Returns a list of all prime factors of 'n'.
+	"""
+	i = 2
+	factors = []
+	while i * i <= n:
+		if n % i:
+			i += 1
+		else:
+			n //= i
+			factors.append(i)
+	if n > 1:
+		factors.append(n)
+	return factors
 
 
 if __name__ == "__main__":
-	print(largest_prime_factor(1000)) #5
-	print(largest_prime_factor(7775460)) #17
+	print(largest_prime_factor(1000)) #should print 5
+	print(all_prime_factors(1000))
+
+	print(largest_prime_factor(7775460)) #should print 17
+	print(all_prime_factors(7775460))
