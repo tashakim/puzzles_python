ee63916b593158c23cae16738c8177b4f42aa279
Tasha Kim <jae_young_kim@brown.edu>
2020-06-24 15:04:22 +0900

    largest prime

---
+++ largest_prime.py
@@ -0,0 +1,13 @@
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
+
+
+if __name__ == "__main__":
+	print(largest_prime_factor(1000)) # 5
+	print(largest_prime_factor(7775460)) # 17
