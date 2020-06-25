fa7cf65f75102553059de12eee0584133a4218e7
Tasha Kim <jae_young_kim@brown.edu>
2020-06-25 23:46:28 +0900

    haystack elapsed

--- pymax.py
+++ pymax.py
@@ -1,11 +1,12 @@
 import time
+
 n = int(input("Number of elements: "))
 haystack = [k for k in range(n)]
 
-print("Searching for the maximum value...")
+print("Searching for maximum value...")
 
 ts = time.time()
-maximum = max(haystack)
+max_val = max(haystack)
 elapsed = time.time() - ts
 
-print("Maximum element = %d, Elapsed time = %.2f" % (maximum, elapsed))
+print("Maximum element = %d, Elapsed time = %.2f" % (max_val, elapsed))
