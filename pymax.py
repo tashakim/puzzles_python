f7d379b6a82936ff87696fd2419e1fe37731bef3
Tasha Kim <jae_young_kim@brown.edu>
2020-06-26 07:20:15 +0900

    maxval in haystack

--- pymax.py
+++ pymax.py
@@ -8,7 +8,7 @@ haystack = [k for k in range(n)]
 print("Searching for maximum value...")
 
 ts = time.time()
-max_val = max(haystack)
-elapsed = time.time() - ts
+max_val = max(haystack) 
+elapsed = time.time() - ts # time elapsed to find 'max_val' in 'haystack'.
 
 print("Maximum element = %d, Elapsed time = %.2f" % (max_val, elapsed))
