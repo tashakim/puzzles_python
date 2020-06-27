83f5baaf98e7711c9e021bcf82eb9613d8475095
Tasha Kim <jae_young_kim@brown.edu>
2020-06-27 21:22:53 +0900

    doublylinked stack

---
+++ linkedlistStack.py
@@ -0,0 +1,19 @@
+class LinkedListStack:
+	def __init__(self):
+		self.data = DoublyLinkedList()
+
+	def size(self):
+		return self.data.getLength()
+
+	def isEmpty(self):
+		return self.size() == 0
+
+	def push(self, item):
+		node = Node(item)
+		self.data.insertAt(self.size() +1, node)
+
+	def pop(self):
+		return self.data.popAt(self.size())
+
+	def peek(self):
+		return self.data.getAt(self.size().data)
