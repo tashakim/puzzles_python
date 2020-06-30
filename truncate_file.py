3a00a02605cde7a21328236861d8f8f7b6217650
Tasha Kim <jae_young_kim@brown.edu>
2020-06-30 22:39:43 +0900

    truncating file

---
+++ truncate_file.py
@@ -0,0 +1,9 @@
+file = open("sample_text.txt", "r")
+#f.truncate(10)
+file.truncate()
+file.close()
+
+#opens and reads the file after the truncate:
+file = open("sample_text.txt", "r")
+print(f.read())
+
