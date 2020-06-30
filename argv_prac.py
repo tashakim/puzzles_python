f47aeeb85cae38ba1ffb05f42ef304ff64aabdbc
Tasha Kim <jae_young_kim@brown.edu>
2020-06-30 21:34:30 +0900

    argv prac

---
+++ argv_prac.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first_arg, second_arg, third_arg = argv
+
+print("Script name: ", script)
+print("First arg: ", first_arg)
+print("Second arg: ", second_arg)
+print("Third arg: ", third_arg)
