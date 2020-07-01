e2cd9a9faba46571f3f26a43496fcbd239287fb4
Tasha Kim <jae_young_kim@brown.edu>
2020-07-01 23:30:56 +0900

    rounds

--- roundingTime.py
+++ roundingTime.py
@@ -3,3 +3,10 @@ from datetime import datetime
 def roundTime(dt = None, roundTo = 60):
 	"""Purpose: 
 	"""
+	if(dt == None): 
+		dt = datetime.datetime.now()
+	seconds = (dt.replace(tzinfo = None) - dt.min).seconds
+	rounding = (seconds + roundTo/2) // roundTo * roundTo
+	return dt + datetime.timedelta(0, rounding-seconds, -dt.microsecond)
+
+if __name__ == "__main__":
