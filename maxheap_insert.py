1f206a7ae2544de2cedbd0a18465c57d035612e2
Tasha Kim <jae_young_kim@brown.edu>
2020-07-09 15:59:44 +0900

    maxheap insert imp

---
+++ maxheap_insert.py
@@ -0,0 +1,12 @@
+class MaxHeap:
+	def__init__(self):
+		self.data = [None]
+
+	def insert(self, item):
+		self.data.append(item)
+		i = len(self.data)-1
+		while(i > 1): # while index of item is NOT the root
+			if(self.data[i] > self.data[i//2]):
+				self.data[i], self.data[i//2] = self.data[i//2], self.data[i]
+			else:
+				return
