c3fea31b1c5951d1155c894e4f933c200ba212a3
Tasha Kim <jae_young_kim@brown.edu>
2020-07-13 16:54:52 +0900

    Add tests for kth max

--- kth_largest.py
+++ kth_largest.py
@@ -2,19 +2,26 @@ class Solution:
     def __init__(self):
         self.stack = []
         
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest(self, nums: List[int], k: int) -> int: # exceeds time limit
         for item in nums:
             self.downstack(item)
-        print(self.stack)
         return self.stack[-k]
             
     def downstack(self, item):
         if self.stack == []:
             self.stack.append(item)
+            return self.stack
         if item >= self.stack[-1]:
             self.stack.append(item)
         else:
             popped = self.stack.pop()
-            downStack(self.stack,item)
+            self.downstack(item)
             self.stack.append(popped)
         return self.stack
+
+if __name__ == "__main__":
+    s = Solution()
+    assert(s.findKthLargest([3,2,1,5,6,4], 2) == 2), "Wrong answer"
+    assert(s.findKthLargest([1,2], 2 ) == 1), "Wrong answer"
+    assert(s.findKthLargest([3,2,3,1,2,4,5,5,6], 4) == 4), "Wrong answer"
+    print("All tests passed!")
