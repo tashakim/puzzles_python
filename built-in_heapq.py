bb351ef8922e5eb8456fe2da8c3b9363cf3cb96f
Tasha Kim <jae_young_kim@brown.edu>
2020-07-14 15:58:12 +0900

    Create maax-heap with built-in module

--- built-in_heapq.py
+++ built-in_heapq.py
@@ -5,4 +5,23 @@ mylist = [1,5,0,10,25,6,70,9,12]
 for x in mylist:
 	heapq.heappush(myheap, x) # Create min-heap
 print(heapq.heappop(myheap)) # Pop min item in heap
-print(myheap) # resulting min-heap
+print(myheap) # Resulting min-heap
+
+print(heapq.nsmallest(5, myheap)) # Restricts heap size to k=5
+
+
+print()
+nums = [4,3,6,1,80,12,88,9]
+heapq.heapify(nums)
+print("My heap as min-heap:", nums)
+
+nums2 = [4,3,6,1,80,12,88,9]
+print("My heap with size 3 restriction: ",heapq.nlargest(3, nums))
+
+
+"""How do we create a max-heap?
+Answer: revert your keys, then call heapify().
+"""
+nums3 = [4,3,6,1,80,12,88,9]
+heapq.heapify([-x for x in nums3])
+print("My heap as max-heap: ", nums3)
