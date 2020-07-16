dfd53f51cc4abe355b13f7dce658642249c40555
Tasha Kim <jae_young_kim@brown.edu>
2020-07-16 23:38:04 +0900

    Implement findMax in bintree recursively

--- findMax_bintree.py
+++ findMax_bintree.py
@@ -7,15 +7,11 @@ def findMax(root):
 	if root == None:
 		return float('-inf')
 
-	leftmax = findMax(root.left)
-	rightmax = findMax(root.right)
+	leftmax = findMax(root.left) # recurse on left subtree
+	rightmax = findMax(root.right) # recurse on right subtree
 
-	if leftmax > root.value:
-		return leftmax
-	elif rightmax > root.value:
-		return rightmax
-	else:
-		return root.value
+	m = max(leftmax, rightmax)
+	return max(m, root.value) # return max. from root, left subtree, and right subtree.
 
 if __name__ == "__main__":
 	root = Node(2)
