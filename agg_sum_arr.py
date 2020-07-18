7595e8497536c48b633c73b0f596b0cd08ddf0f5
Tasha Kim <jae_young_kim@brown.edu>
2020-07-19 00:04:41 +0900

    Computes aggregate sum

---
+++ agg_sum_arr.py
@@ -0,0 +1,20 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+    """Purpose: Computes aggregate sum of array 'nums'.
+    """
+        sums = 0
+        l = []
+        for x in nums:
+            sums += x
+            l.append(sums)
+        return l
+    
+    def runningSum2(self, nums: List[int]) -> List[int]:
+    """Purpose: T.C: O(n), S.C: O(1).
+    Q: Why is this method faster than runningSum() above?
+    """
+        i = 1
+        while i<len(nums):
+            nums[i] += nums[i-1]
+            i += 1
+        return nums
