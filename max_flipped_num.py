18336dcb79aef3e9197a10c0754b052578f69c50
Tasha Kim <jae_young_kim@brown.edu>
2020-07-19 23:47:03 +0900

    Correct sol for max flipped

---
+++ max_flipped_num.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        s, count = [],0
+        num = str(num)
+        for i in range(len(num)):
+            print(s)
+            if num[i] == '9':
+                s.append('9')
+                count += 1
+            elif num[i] == '6':
+                s.append('9')
+                break
+        for i in range(count+1, len(num)):
+            s.append(str(num[i]))
+        return "".join(s)
