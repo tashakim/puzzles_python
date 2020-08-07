193ec268c1be811448f86b4c0d29dc6a11b953d9
Tasha Kim <jae_young_kim@brown.edu>
2020-08-07 22:48:06 +0900

    Add staircase problem

--- strict_inc_blocks.py
+++ strict_inc_blocks.py
@@ -1,6 +1,10 @@
-def blocks():
+def blocks(n):
 	"""Purpose: Returns the max no. of possible staircases, given n, number of blocks.
 	"""
 
 
+
 if __name__ == "__main__":
+	assert(blocks(3) == 1), "Wrong"
+	assert(blocks(4) == 1), "Wrong"
+	assert(blocks(5) == 2), "Wrong"
