3180178fb3159e32e3ff1c7f1ebc9f91db9baf74
Tasha Kim <jae_young_kim@brown.edu>
2020-08-19 22:58:30 +0900

    Improve space complexity by a factor of number of nodes

--- palindrome_linkedlist.py
+++ palindrome_linkedlist.py
@@ -7,6 +7,11 @@
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
+        """Purpose: Checks if linked list values form a palindrome.
+
+        Stores all values of linked list in an array,
+        then reverses the array.
+        """
         dummy = ListNode(-1)
         dummy.next = head
         cur = dummy.next
@@ -23,3 +28,22 @@ class Solution:
             head += 1
             tail -= 1
         return True
+
+    def isPalindrome(self, head):
+        """Purpose: Same as above, but with improved space complexity.
+
+        In-place solution, using slow & fast 
+        pointers to find middle point efficiently.
+        """
+        rev = None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            slow = slow.next
+            rev = rev.next
+            
+        return not rev
