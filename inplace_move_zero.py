8c9408adba52971a09890b6c3b89e52d955f01db
Tasha Kim <jae_young_kim@brown.edu>
2020-08-19 23:27:49 +0900

    Move all zeroes to the back in-place

---
+++ inplace_move_zero.py
@@ -0,0 +1,16 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Purpose: Move all occurrences of zeros to the back of array, 
+        note: in-place solution. 
+
+        Do not return anything, modify nums in-place instead.
+        """
+        count= nums.count(0)
+        for k in range(count):
+            i = nums.index(0)
+            while i < len(nums) - 1:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                i += 1
+                
+        return nums
