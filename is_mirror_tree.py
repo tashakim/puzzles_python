27410ac58aae8ec55ccc5f3771ddc3f8812fbcbf
Tasha Kim <jae_young_kim@brown.edu>
2020-08-20 20:25:44 +0900

    Cut out unnecessary duplicate computation on root node

--- is_mirror_tree.py
+++ is_mirror_tree.py
@@ -17,3 +17,18 @@ class Solution:
             return t1.val == t2.val and isMirror(t1.right, t2.left) and isMirror(t1.left, t2.right)
     
         return isMirror(root.left, root.right)
+
+
+     def isSymmetric1(self, root):
+     	"""Returns same as above. 
+
+     	Note: Cuts out unnecessary duplicate computation on root node.
+     	"""
+     	def isMirror(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return t1.val == t2.val and isMirror(t1.right, t2.left) and isMirror(t1.left, t2.right)
+    
+        return isMirror(root.left, root.right)
