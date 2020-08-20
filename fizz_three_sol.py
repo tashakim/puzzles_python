decad8c71f492a97924940b7c296a1b33046a71a
Tasha Kim <jae_young_kim@brown.edu>
2020-08-20 20:30:52 +0900

    Return correct result using string concatenation instead of conditions

--- fizz_three_sol.py
+++ fizz_three_sol.py
@@ -12,3 +12,19 @@ class Solution:
             else:
                 res.append(str(i))
         return res
+
+    def fizzBuzz(self, n):
+        # String concatenation! 
+        # TC: O(n
+        ans = []
+        
+        for i in range(1, n+1):
+            res = ""
+            if i%3 == 0:
+                res += "Fizz"
+            if i%5 == 0:
+                res += "Buzz"
+            if not (i%3==0 or i%5==0):
+                res += str(i)
+            ans.append(res)
+        return ans
