81a47924f098a1787d94b05942b610ba2be5d525
Tasha Kim <jae_young_kim@brown.edu>
2020-08-24 13:50:02 +0900

    Develop tests for hamming distance function

--- hamming_dist.py
+++ hamming_dist.py
@@ -6,4 +6,19 @@ class Solution:
     computes the number of bit positions in which two bits are different.
     """
         s = str(bin(x^y)[2:])
+
         return s.count('1')
+
+def test():
+	assert(hammingDistance(0,0) == 0), "Hamming distance between two equal integers equal zero."
+
+	assert(hammingDistance(0,4) == 1), "Hamming distance between zero and another integer equal the number of 1's in its binary representation."
+	
+	assert(hammingDistance(1,4) == 2), "Wrong answer"
+	assert(hammingDistance(200,400) == 4), "Wrong answer"
+	assert(hammingDistance(0,1015) == 9), "Wrong answer"
+
+
+if __name__ == "__main__":
+	test()
+	print("All tests passed!")
