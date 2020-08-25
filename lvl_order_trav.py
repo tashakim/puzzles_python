583c731b154c373ea7425cbeb9dc6d7577a22363
Tasha Kim <jae_young_kim@brown.edu>
2020-08-25 22:08:09 +0900

    Uncomment TreeNode class used for traversal

--- lvl_order_trav.py
+++ lvl_order_trav.py
@@ -1,9 +1,8 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
@@ -24,3 +23,5 @@ class Solution:
         # BFs order printed.
 
         # print grouped by level here:
+
+        
