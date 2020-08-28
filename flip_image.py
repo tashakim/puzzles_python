80fd83117e0e05b49f8b00042efdb8a4e1ff6505
Tasha Kim <jae_young_kim@brown.edu>
2020-08-29 00:05:17 +0900

    Improve space complexity using in-place swapping within matrix

--- flip_image.py
+++ flip_image.py
@@ -12,4 +12,12 @@ class Solution:
                 
         return dp
 
-       
+    def flipAndInvertImage(self, A):
+        """Purpose: Improves space complexity of the above method,
+        by swapping items in-place.
+        """
+        for row in range(len(A + 1)//2):
+            for i in range(len(row)):
+                A[i], A[-i-1] = A[-i-1]^1, A[-i]^1
+
+        return A
