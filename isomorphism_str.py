31023b41981e59c51d9524986a6a3572b085a67c
Tasha Kim <jae_young_kim@brown.edu>
2020-09-02 19:33:25 +0900

    Construct isomorphism checker for words and strings

---
+++ isomorphism_str.py
@@ -0,0 +1,23 @@
+class Isomorphism:
+	def wordPattern(self, pattern, strings):
+		"""Purpose: Return whether or not there is 
+		a bijection between a letter in pattern, 
+		and a non-empty word in strings.
+		"""
+
+
+
+
+
+	def stringPattern(self, p, q):
+		"""Purpose: Return whether or not the characters
+		in p can be replaced to get q.
+		
+		Example: 
+		p, q = "paper", "adame" => True
+		p, q = "hey", "boo" => False
+		"""
+		unique_chars_in_p = 
+		unique_chars_in_q = 
+		unique_pairs_in_pq = 
+		return unique
