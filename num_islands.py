c326343ebc12fbd46a6e03532cf7ae39971d10e3
Tasha Kim <jae_young_kim@brown.edu>
2020-09-04 17:03:09 +0900

    Add test and description

--- num_islands.py
+++ num_islands.py
@@ -1,5 +1,8 @@
-class Solution:
+class Solution():
 	def numIslands(self, grid):
+		"""Purpose: Return the number of islands of '1', given a
+		2d graph input filled by '1' or '0'.
+		"""
 		if not grid: return 0
 		count = 0
 		for i in range(len(grid)):
@@ -17,3 +20,10 @@ class Solution:
 		self.dfs(grid, i-1, j)
 		self.dfs(grid, i, j+1)
 		self.dfs(grid, i, j-1)
+
+
+if __name__ == "__main__":
+	grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+	s= Solution()
+	assert(s.numIslands(grid) == 1), "Wrong answer"
+	print("All tests passed!")
