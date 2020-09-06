d949edac94836856bf743d4c79953a9411403a21
Tasha Kim <jae_young_kim@brown.edu>
2020-09-06 21:30:39 +0900

    Implement a known search method using recursion

--- recursive_problem_solving.py
+++ recursive_problem_solving.py
@@ -10,7 +10,6 @@ class Recursion:
 		return self.power(num, pwr-1) * num
 
 
-
 	def fibonacci(self, num):
 		"""Purpose:
 		"""
@@ -24,6 +23,7 @@ class Recursion:
 
 		return self.fibonacci(num-1) + self.fibonacci(num-2)
 
+
 	def factorial(self, num):
 		"""Purpose:
 		"""
@@ -33,10 +33,25 @@ class Recursion:
 		return
 
 
+	def findItem(self, data, item):
+		"""Purpose:
+		"""
+
+		# Breaking condition
+		if len(data) == 1:
+			return data.pop() == item
+
+		if item == data[-1]:
+			return True
+
+		return self.findItem(data[:-1], item)
+
+
 if __name__ == "__main__":
 	r = Recursion()
 	assert(r.power(2, 6) == 64), "Wrong value"
 	assert(r.fibonacci(5) == 5), "Wrong value"
 	assert(r.fibonacci(6) == 8), "Wrong value"
+	assert(r.findItem([1,2,3,4], 5) == False), "Wrong value"
 
 	print("Tests passed!")
