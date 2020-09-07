028d57e11d964fd92b691f0fade42869f4b6489d
Tasha Kim <jae_young_kim@brown.edu>
2020-09-07 23:14:32 +0900

    Create memento

---
+++ memento_ex.py
@@ -0,0 +1,13 @@
+import pickle # allows serializing & deserializing python objects
+
+class Originator:
+	def __init__(self):
+
+	def create_memento(self):
+
+	def set_memento(self, memento):
+
+def main():
+
+
+main()
