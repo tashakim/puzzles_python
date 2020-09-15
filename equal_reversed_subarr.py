e9536baf895c1e4bf67519508c6c86bd8fabd265
Tasha Kim <jae_young_kim@brown.edu>
2020-09-15 23:30:27 +0900

    Add method description

--- equal_reversed_subarr.py
+++ equal_reversed_subarr.py
@@ -1,3 +1,11 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+    	"""Purpose: Given two integer arrays of equal length 
+    	target, arr,
+    	select any non-empty subarry of arr and reverse it, any
+    	number of steps.
+    	Return boolean indicating whether arr equals to target.
+
+    	
+    	"""
         return collections.Counter(target) == collections.Counter(arr)
