beb8631cbc415fbaf0699e09ee8c3e7f6c4323b8
Tasha Kim <jae_young_kim@brown.edu>
2020-09-17 21:03:00 +0900

    Alternate sort

---
+++ alternate_sort.py
@@ -0,0 +1,12 @@
+def alternatingSort(a):
+    cur, prev = 0, float('-inf')
+    for i in range(len(a)):
+        print("i = ", i)
+        if i%2 == 0: # even index
+            cur = i//2
+        else: # odd index
+            cur = (-i//2) 
+        if a[cur] <= prev:
+            return False
+        prev = a[cur]
+    return True
