b854b07ad4679c54aafdb24bbe3a591310464d5f
Tasha Kim <tasha_kim@brown.edu>
2021-06-05 22:02:03 -0400

    Reversing & symmetry methods

--- partitionLL.py
+++ partitionLL.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solutions:
     def deleteDuplicateNode(self, head: ListNode) -> ListNode:
         # Purpose: Deletes a single duplicate node, if it exists.
@@ -108,6 +109,94 @@ class Solutions:
         """
 
 
+    def isPalindrome(self, head: ListNode) -> bool:
+        # Purpose: Checks whether values in linked list forms a palindrome. 
+        # Note space complexity is O(n), n = length 0f list.
+        storage = []
+        ptr = head
+
+        while ptr:
+            storage.append(ptr.val)
+            ptr = ptr.next
+        i = 0
+        while i < len(storage) / 2:
+            if storage[i] != storage[-i-1]:
+                return False
+            i += 1
+
+        return True
+
+
+    def isPalindrome(self, head: ListNode) -> bool:
+        # Purpose: Checks if linked list forms a palindrome, with O(1) space complexity.
+        self.head = head
+        def recursiveCheck(cur = head):
+            if cur:
+                if not recursiveCheck(cur.next):
+                    return False
+                if self.head.val != cur.val:
+                    return False
+                self.head = self.head.next
+            return True
+        
+        return recursiveCheck()
+
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Purpose: Reverses the values in a linked list.
+        prev = None # will become the val after TAIL node.
+        cur = head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev # return from the back
+
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Purpose: Reverses the values in a linked list, WITHOUT multiple assignment.
+        prev_node = None
+        cur_node = head
+        while cur_node:
+            next_node = cur_node.next # copy over before we dismantle the bridge!
+            cur_node.next = prev_node
+            prev_node = cur_node
+            cur_node = cur_node.next
+
+        return prev_node 
+        """
+        head = prev_node
+        return head
+        """
+
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Purpose: Reverses the values in a linked list, using an Array.
+        # Space complexity is increased to O(n), n = length of linked list.
+        storage = []
+        cur = head
+        while cur:
+            storage.append(cur.val)
+            cur = cur.val
+        
+        storage.reverse() # reversed_storage = list(reversed(storage))
+        cur = head
+        i = 0
+        while cur:
+            cur.val = storage[i] # don't reverse while looping! Save time.
+            cur = cur.next
+            i += 1
+        return head
+
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Purpose: Reverses the values in a linked list, recursively.
+        if not head or if not head.next:
+            return head
+        res = self.reverselList(head.next) 
+        head.next.next = head
+        head.next = None
+        return res
+
+
 class Solution(object):
     def partition(self, head, x):
         """
@@ -115,32 +204,21 @@ class Solution(object):
         :type x: int
         :rtype: ListNode
         """
-
-        # before and after are the two pointers used to create two list
-        # before_head and after_head are used to save the heads of the two lists.
-        # All of these are initialized with the dummy nodes created.
-        before = before_head = ListNode(0)
-        after = after_head = ListNode(0)
+        lower_head = ListNode(0, head)
+        higher_head = ListNode(0, head)
+        lower = lower_head
+        higher = higher_head
 
         while head:
-            # If the original list node is lesser than the given x,
-            # assign it to the before list.
             if head.val < x:
-                before.next = head
-                before = before.next
+                lower.next = head
+                lower = lower.next
             else:
-                # If the original list node is greater or equal to the given x,
-                # assign it to the after list.
-                after.next = head
-                after = after.next
-
-            # move ahead in the original list
+                higher.next = head
+                higher = higher.next
             head = head.next
 
-        # Last node of "after" list would also be ending node of the reformed list
-        after.next = None
-        # Once all the nodes are correctly assigned to the two lists,
-        # combine them to form a single list which would be returned.
-        before.next = after_head.next
+        higher.next = None
+        lower.next = lower_head.next
 
-        return before_head.next
+        return lower_head.next
