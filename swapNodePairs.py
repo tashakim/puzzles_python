3a62777eba6776506c4df35538fcf82a56c8706b
Tasha Kim <tasha_kim@brown.edu>
2021-07-11 20:04:50 -0400

    Add iterative solution

--- swapNodePairs.py
+++ swapNodePairs.py
@@ -1,6 +1,6 @@
 def swapNodePairs(head: ListNode) -> ListNode:
-    # Purpose: Swaps nodes in a linked list, in pairs.
-    
+    # Purpose: Recursively swaps nodes in a linked list, in pairs.
+
     if not head or head.next:
         return head
     first = node
@@ -10,3 +10,22 @@ def swapNodePairs(head: ListNode) -> ListNode:
     second.next = first
 
     return second
+
+
+def swapNodePairs(head: ListNode) -> ListNode:
+    # Purpose: Iteratively swaps nodes in a linked list, in pairs.
+    
+    dummy = ListNode(-1)
+    dummy.next = head
+    dummy = prev
+
+    while head and head.next:
+        first = head
+        second = head.next
+
+        prev.next, first.next, second.next = second, second.next, first
+
+        prev = first
+        head = first.next
+
+    return dummy.next
