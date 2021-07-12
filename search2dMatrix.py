3fabd803d492d92e31b822e2fe109e1a064b34b6
Tasha Kim <tasha_kim@brown.edu>
2021-07-11 22:27:00 -0400

    Update sol

--- search2dMatrix.py
+++ search2dMatrix.py
@@ -2,7 +2,7 @@ class Solution:
     def searchMatrix(self, matrix, target):
         """
         Purpose: searches 2D matrix, and returns True if target is found.
-        
+
         Time complexity: O(n*n)
         """
         for i in range(len(matrix)):
@@ -30,7 +30,7 @@ class Solution:
         """
         Purpose: Binary searches 2D matrix, and returns True if target is found.
         
-        Time complexity: O(log(n))
+        Time complexity: O(log(n*m))
         """
         res = []
         for x in matrix:
@@ -50,3 +50,24 @@ class Solution:
                 left = mid + 1
 
         return False 
+
+
+    def searchMatrix3(self, matrix, target):
+        """
+        Purpose: Binary searches 2D matrix, and returns True if target is found.
+        
+        Time complexity: O(log(n*m))
+        """
+        n = len(matrix)
+        m = len(matrix[0])
+        left, right = 0, (n * m) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target == matrix[mid // m][mid % m]:
+                return True
+            elif target < matrix[mid // m][mid % m]:
+                right = mid - 1
+            else: 
+                left = mid + 1
+
+        return False
