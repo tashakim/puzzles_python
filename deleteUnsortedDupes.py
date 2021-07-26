93d9d8ef5470f3bdbf8f3043e97fb63f4c585362
Tasha Kim <tasha_kim@brown.edu>
2021-07-25 22:14:07 -0400

    Manual counter

--- deleteUnsortedDupes.py
+++ deleteUnsortedDupes.py
@@ -1,8 +1,11 @@
+import collections
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def deleteDuplicatesUnsorted(self, head: ListNode) -> ListNode:
         res =[]
@@ -12,10 +15,19 @@ class Solution:
             cur = cur.next
         cache = collections.Counter(res)
         
+        """ Manual Counter:
+        
+        cache = collections.defaultdict(int)
+        cur = head
+        while cur:
+            cache[cur.val] += 1
+            cur = cur.next
+        """
+        
         dummy = ListNode(-1)
         dummy.next = head
         prev = dummy
-        
+
         while head:
             if cache[head.val] > 1:
                 prev.next = head.next
