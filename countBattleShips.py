3fd907e563bd457497f8a0de433e16f26109c83c
Tasha Kim <tasha_kim@brown.edu>
2021-08-10 15:13:46 -0400

    Count hor, ver battleships

--- countBattleShips.py
+++ countBattleShips.py
@@ -4,5 +4,17 @@ class Solution:
         Purpose: Given a m x n matrix board, where each cell indicates part of a battleship
         'X', or empty '.', 
         returns the number of battleships on board.
+
+        Note: Each battleship can only be placed horizontally or vertically on the board.
+
         """
+        count = 0
         
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 'X':
+                    if (i==0 or board[i-1][j]=='.') and (j==0 or board[i][j-1]=='.'):
+                        count += 1
+                        
+        return count
+                        
