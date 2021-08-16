7702356b48b58b6f58c211401f9aec71182c05cd
Tasha Kim <tasha_kim@brown.edu>
2021-08-16 16:29:44 -0400

    Recursive backtracking permutations

--- generatePermutations.py
+++ generatePermutations.py
@@ -32,3 +32,16 @@ def generatePermutations1(self, arr, n):
     p = permutations(arr)
 
     return [x for x in p]
+
+def generatePermutations2(self, arr: List[str], k):
+    """
+    Purpose: 
+    Input: 
+    ["a", "b", "c"], 1   ->    ['a', 'b', 'c']
+    ["a", "b", "c"], 2   ->    ['aa', 'ba', 'ca', 'ab', 'bb', 'cb', 'ac', 'bc', 'cc']
+    ["a", "b", "c"], 3   ->    ['aaa', 'baa', 'caa', 'aba', 'bba', 'cba', 'aca', 'bca', 'cca', 'aab', 'bab', 'cab', 'abb', 'bbb', 'cbb', 'acb', 'bcb', 'ccb', 'aac', 'bac', 'cac', 'abc', 'bbc', 'cbc', 'acc', 'bcc', 'ccc']
+    """
+    if k == 1: 
+        return arr
+    
+    return [y + x for y in generatePermutations2(arr, 1) for x in generatePermutations2(arr, k-1)]
