0e7f276b8b429743c8b00578921b5ac536e51e9b
Tasha Kim <tasha_kim@brown.edu>
2021-08-19 03:31:36 -0400

    Compute diameter in undir. graph

--- backtrackCombinations.py
+++ backtrackCombinations.py
@@ -3,15 +3,36 @@ class Solution:
         """
         Purpose: Generates all combinations of elements in `arr`, using backtracking.
         """
+        def backtrack(arr, k, start, path):
+            if len(path) == k:
+                self.res.append(path[:])
+                return
+            
+            for i in range(start, len(arr)):
+                path.append(arr[i])
+                backtrack(arr, k, start+1, path)
+                path.pop()
+
+        self.res = []
+        backtrack(arr, k, 0, [])
+        
+        return self.res
+
+
+    def genCombinations(arr, k):
+        """
+        Purpose: Generates all INCREASING combinations of elements in `arr`, using backtracking.
+        """
         def backtrack(arr, k, depth, path):
             if len(path) == k:
                 self.res.append(path[:])
                 return
             
             for i in range(depth, len(arr)):
-                path.append(arr[i])
-                backtrack(arr, k, depth+1, path)
-                path.pop()
+                if not path or arr[i] > path[-1]:
+                    path.append(arr[i])
+                    backtrack(arr, k, depth+1, path)
+                    path.pop()
 
         self.res = []
         backtrack(arr, k, 0, [])
@@ -22,6 +43,7 @@ class Solution:
     def genPermutations(arr, k):
         """
         Purpose: Generates all permutations of elements in `arr` using backtracking.
+        Allows no repeat characters.
         """
         def backtrack(arr, k, depth, path, seen):
             if depth == k:
@@ -41,4 +63,26 @@ class Solution:
         self.res = []
         backtrack(arr, k, 0, [], set())
 
+        return self.res
+
+
+    def genPermutations1(arr, k):
+        """
+        Purpose: Generates all permutations of elements in `arr` using backtracking.
+        Allows repeat characters.
+        """
+        def backtrack(arr, k, depth, path):
+            if depth == k:
+                self.res.append(path[:])
+                return
+            
+            for i in range(len(arr)):
+                path.append(arr[i])
+                backtrack(arr, k, depth+1, path)
+                path.pop()
+
+        self.res = []
+
+        backtrack(nums, len(nums), 0, [])
+        
         return self.res
---
+++ undirGraphDiameter.py
@@ -0,0 +1,26 @@
+class Solution:
+    def treeDiameter(self, edges: List[List[int]]) -> int:
+        """
+        Purpose: Returns the diameter (no. of edges in longest path) in an undirected graph.
+        """
+        edges.sort()
+        self.tree = defaultdict(lambda: [])
+        for x,y in edges:
+            self.tree[x].append(y)
+        root = edges[0]
+        self.max_path = 0
+        
+        def traverse(node):
+            if node not in self.tree:
+                return 1
+            subtrees = []
+            for child in self.tree[node]:
+                subtrees.append(traverse(child))
+            pairs = [x for x in itertools.permutations(subtrees, 2)]
+        
+            for x in pairs:
+                self.max_path = max(self.max_path, sum(x))
+            return max(subtrees) + 1 
+        
+        traverse(root[0])
+        return self.max_path
