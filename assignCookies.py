c35342aa91cd8c6260cc2cea5dd150cf4ba64109
Tasha Kim <tasha_kim@brown.edu>
2021-08-22 21:26:33 -0400

    Assign cookies after sorting

---
+++ assignCookies.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findContentChildren(self, g, s):
+        """
+        Purpose: Given greed factor g[i] of each child, returns the max. number
+        of children that are content after distributing cookies of size s[i].
+
+        Note: A child is content if they eat a cookie that has size greater than 
+        or equal to their greed factor.
+
+        Sample Input: g = [1,2,3], s= [1,1]
+        Sample Output: 1
+        """
+        g.sort()
+        s.sort()
+        
+        child = cookie = 0
+        num_cookies = len(s)
+        num_children = len(g)
+        
+        while cookie < num_cookies and child < num_children:
+            if s[cookie] >= g[child]:
+                child += 1
+            cookie += 1
+        
+        return child
